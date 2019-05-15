9b3ad3c9047886c3aa46c2a97512ee6009573d4b
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:08:26 +0530

    Update InputandOutput.cpp

--- InputandOutput.cpp
+++ InputandOutput.cpp
@@ -5,13 +5,15 @@
 #include <algorithm>
 using namespace std;
 
-
-int main() {
+int main() 
+{
     int a,b,c;
     scanf("%d", &a);
     scanf("%d", &b);
     scanf("%d", &c);
     printf("%d",(a+b+c));
+
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+ 
     return 0;
 }
