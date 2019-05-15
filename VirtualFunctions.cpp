73334aa922f5667ab0183a626991c0e2bcbbf7a6
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:46:00 +0530

    Update VirtualFunctions.cpp

--- VirtualFunctions.cpp
+++ VirtualFunctions.cpp
@@ -4,76 +4,100 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-class Person {
-public:
-  string name;
-  int age;
-  int id{};
-  virtual void putdata() = 0;
-  virtual void getdata() = 0;
+
+class Person 
+{
+  public:
+    string name;
+    int age;
+    int id
+    {};
+    
+    virtual void putdata() = 0;
+    virtual void getdata() = 0;
 };
 
-class Professor : public Person {
-private:
-  unsigned int publications;
-
-public:
-  static int cur_id;
-  Professor() { id = ++cur_id; }
-  void putdata() override {
-    cout << name << " " << age << " " << publications << " " << id << endl;
-  }
-  void getdata() override { cin >> name >> age >> publications; }
-};
-
-class Student : public Person {
-private:
-  int marks[6];
-  int sum;
-  // int id;
-
-public:
-  static int cur_id;
-  Student() { id = ++cur_id; }
-  void putdata() override {
-    cout << name << " " << age << " " << sum << " " << id << endl;
-  }
-
-  void getdata() override {
-    cin >> name >> age;
-    for (int i = 0; i < 6; i++) {
-      cin >> marks[i];
-      sum += marks[i];
+class Professor : public Person 
+{
+  private:
+    unsigned int publications;
+  public:
+    static int cur_id;
+    Professor() 
+    { id = ++cur_id; }
+    
+    void putdata() override 
+    {
+      cout << name << " " << age << " " << publications << " " << id << endl;
     }
-  }
+    void getdata() override 
+    { 
+      cin >> name >> age >> publications; 
+    }
+};
+
+class Student : public Person 
+{
+  private:
+    int marks[6];
+    int sum;
+    
+    // int id;
+
+  public:
+    static int cur_id;
+    Student() 
+    { id = ++cur_id; }
+  
+    void putdata() override 
+    {
+      cout << name << " " << age << " " << sum << " " << id << endl;
+    }
+
+    void getdata() override 
+    {
+      cin >> name >> age;
+      for (int i = 0; i < 6; i++) 
+      {
+        cin >> marks[i];
+        sum += marks[i];
+      }
+    }
 };
 
 int Professor::cur_id = 0;
 int Student::cur_id = 0;
 
-int main(){
-
+int main()
+{
     int n, val;
-    cin>>n; //The number of objects that is going to be created.
+    cin>>n; 
+  
+  //The number of objects that is going to be created.
+  
     Person *per[n];
-
-    for(int i = 0;i < n;i++){
-
+    for(int i = 0;i < n;i++)
+    {
         cin>>val;
-        if(val == 1){
-            // If val is 1 current object is of type Professor
-            per[i] = new Professor;
-
+        if(val == 1)
+        {
+          
+          // If val is 1 current object is of type Professor
+          
+          per[i] = new Professor;
         }
-        else per[i] = new Student; // Else the current object is of type Student
+        else per[i] = new Student; 
+      
+      // Else the current object is of type Student
 
-        per[i]->getdata(); // Get the data from the user.
-
+        per[i]->getdata(); 
+      
+      // Get the data from the user.
     }
-
     for(int i=0;i<n;i++)
-        per[i]->putdata(); // Print the required output for each object.
+        per[i]->putdata(); 
+  
+  // Print the required output for each object.
 
     return 0;
-
 }
