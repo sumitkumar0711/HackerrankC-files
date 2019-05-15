c8dd8bb88a6f486a90402a6ba8c1ce7c449aa1ec
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 16:44:16 +0530

    Update ExceptionalServer.cpp

--- ExceptionalServer.cpp
+++ ExceptionalServer.cpp
@@ -6,44 +6,58 @@
 #include <cmath>
 using namespace std;
 
-class Server {
-private:
-	static int load;
-public:
-	static int compute(long long A, long long B) {
-		load += 1;
-		if(A < 0) {
-			throw std::invalid_argument("A is negative");
+class Server 
+{
+	private:
+		static int load;
+	public:
+		static int compute(long long A, long long B) 
+		{
+			load += 1;
+			if(A < 0) 
+			{
+				throw std::invalid_argument("A is negative");
+			}
+			vector<int> v(A, 0);
+			int real = -1, cmplx = sqrt(-1);
+			if(B == 0) throw 0;
+			real = (A/B)*real;
+			int ans = v.at(B);
+			return real + A - B*ans;
 		}
-		vector<int> v(A, 0);
-		int real = -1, cmplx = sqrt(-1);
-		if(B == 0) throw 0;
-		real = (A/B)*real;
-		int ans = v.at(B);
-		return real + A - B*ans;
-	}
-	static int getLoad() {
+	static int getLoad() 
+	{
 		return load;
 	}
 };
+
 int Server::load = 0;
 
-int main() {
+int main() 
+{
 	int T; cin >> T;
-	while(T--) {
+	while(T--) 
+	{
 		long long A, B;
 		cin >> A >> B;
-                try {
+                try 
+		{
                   cout << Server::compute(A, B) << endl;
-                } catch (bad_alloc &error) {
+                } 
+		catch (bad_alloc &error) 
+		{
                   cout << "Not enough memory" << endl;
-                } catch (exception &error) {
+                } 
+		catch (exception &error) 
+		{
                   cout << "Exception: " << error.what() << endl;
-                } catch (...) {
+                }
+		catch (...) 
+		{
                   cout << "Other Exception" << endl;
                 }
-                /* Enter your code here. */
-
+                
+		/* Enter your code here. */
 	}
 	cout << Server::getLoad() << endl;
 	return 0;
