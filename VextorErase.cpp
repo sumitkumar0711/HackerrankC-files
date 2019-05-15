60856abf712f90644bec2000224a5eeb25262888
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:43:02 +0530

    Update VextorErase.cpp

--- VextorErase.cpp
+++ VextorErase.cpp
@@ -5,27 +5,25 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-  vector<int> v{};
+int main() 
+{
+  vector<int> v
+  {};
   int vSize;
-
   cin >> vSize;
-  for (int i = 0; i < vSize; i++) {
+  for (int i = 0; i < vSize; i++) 
+  {
     int num;
     cin >> num;
     v.push_back(num);
   }
-
   int a, b;
   cin >> a;
   v.erase(begin(v) + a - 1);
   cin >> a >> b;
   v.erase(begin(v) + a - 1, begin(v) + b - 1);
-
   cout << v.size() << endl;
   for_each(v.begin(), v.end(), [](int i) { cout << i << " "; });
   cout << endl;
-
   return 0;
 }
-
