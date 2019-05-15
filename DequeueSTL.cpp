f827280bb3419759321a1f4dba0ddc4cb3e0d53d
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 16:42:00 +0530

    Update DequeueSTL.cpp

--- DequeueSTL.cpp
+++ DequeueSTL.cpp
@@ -2,15 +2,18 @@
 #include <deque> 
 using namespace std;
 
-void printmax(int *a, int b, int c) {
+void printmax(int *a, int b, int c) 
+{
   std::deque<int> dq;
   int y;
-  for (y = 0; y < c; y++) {
+  for (y = 0; y < c; y++) 
+  {
     while ((!dq.empty()) && a[y] >= a[dq.back()])
       dq.pop_back();
     dq.push_back(y);
   }
-  for (; y < b; y++) {
+  for (; y < b; y++) 
+  {
     std::cout << a[dq.front()] << " ";
     while ((!dq.empty()) && dq.front() <= y - c)
       dq.pop_front();
@@ -21,14 +24,17 @@ void printmax(int *a, int b, int c) {
   std::cout << a[dq.front()];
 }
 
-int main() {
+int main() 
+{
   std::ios_base::sync_with_stdio(false);
   int i, j, k, n;
   std::cin >> i;
-  while (i--) {
+  while (i--) 
+  {
     std::cin >> j >> k;
     int m[j] = {};
-    for (int &l : m) {
+    for (int &l : m) 
+    {
       std::cin >> n;
       l = n;
     };
