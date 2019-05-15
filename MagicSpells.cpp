f05b56bd913ce06d6bec362ce77b87b9b0a4feee
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:19:11 +0530

    Update MagicSpells.cpp

--- MagicSpells.cpp
+++ MagicSpells.cpp
@@ -3,117 +3,164 @@
 #include <string>
 using namespace std;
 
-class Spell { 
+class Spell 
+{ 
     private:
         string scrollName;
     public:
-        Spell(): scrollName("") { }
-        Spell(string name): scrollName(name) { }
-        virtual ~Spell() { }
-        string revealScrollName() {
+        Spell(): scrollName("") 
+        { }
+        
+        Spell(string name): scrollName(name) 
+        { }
+        
+        virtual ~Spell() 
+        { }
+        
+        string revealScrollName() 
+        {
             return scrollName;
         }
 };
 
-class Fireball : public Spell { 
+class Fireball : public Spell 
+{ 
     private: int power;
     public:
-        Fireball(int power): power(power) { }
-        void revealFirepower(){
+        Fireball(int power): power(power) 
+        { }
+        
+        void revealFirepower()
+        {
             cout << "Fireball: " << power << endl;
         }
 };
 
-class Frostbite : public Spell {
+class Frostbite : public Spell 
+{
     private: int power;
     public:
-        Frostbite(int power): power(power) { }
-        void revealFrostpower(){
+        Frostbite(int power): power(power) 
+        { }
+        
+        void revealFrostpower()
+        {
             cout << "Frostbite: " << power << endl;
         }
 };
 
-class Thunderstorm : public Spell { 
+class Thunderstorm : public Spell 
+{ 
     private: int power;
     public:
-        Thunderstorm(int power): power(power) { }
-        void revealThunderpower(){
+        Thunderstorm(int power): power(power) 
+        { }
+        void revealThunderpower()
+        {
             cout << "Thunderstorm: " << power << endl;
         }
 };
 
-class Waterbolt : public Spell { 
+class Waterbolt : public Spell 
+{ 
     private: int power;
     public:
-        Waterbolt(int power): power(power) { }
-        void revealWaterpower(){
+        Waterbolt(int power): power(power) 
+        { }
+        void revealWaterpower()
+        {
             cout << "Waterbolt: " << power << endl;
         }
 };
 
-class SpellJournal {
+class SpellJournal 
+{
     public:
         static string journal;
-        static string read() {
+        static string read() 
+        {
             return journal;
         }
 }; 
+
 string SpellJournal::journal = "";
 
-void counterspell(Spell *spell) {
+void counterspell(Spell *spell) 
+{
 
-  /* Enter your code here */ if (Fireball *fb =
-                                     dynamic_cast<Fireball *>(spell)) {
-    fb->revealFirepower();
-  } else if (Frostbite *fz = dynamic_cast<Frostbite *>(spell)) {
-    fz->revealFrostpower();
-  } else if (Thunderstorm *ts = dynamic_cast<Thunderstorm *>(spell)) {
-    ts->revealThunderpower();
-  } else if (Waterbolt *wb = dynamic_cast<Waterbolt *>(spell)) {
-    wb->revealWaterpower();
-  } else { // generic spell
-    string spellN = spell->revealScrollName();
-    string spellJ = SpellJournal::read();
-    int m = spellN.length();
-    int n = spellJ.length();
-    int array[m + 1][n + 1];
+  /* Enter your code here */ 
+    if (Fireball *fb = dynamic_cast<Fireball *>(spell)) 
+    {
+        fb->revealFirepower();
+    }
+    else if (Frostbite *fz = dynamic_cast<Frostbite *>(spell)) 
+    {
+        fz->revealFrostpower();
+    }
+    else if (Thunderstorm *ts = dynamic_cast<Thunderstorm *>(spell)) 
+    {
+        ts->revealThunderpower();
+    }
+    else if (Waterbolt *wb = dynamic_cast<Waterbolt *>(spell)) 
+    {
+        wb->revealWaterpower();
+    } 
+    else { 
+        
+        // generic spell
+        
+        string spellN = spell->revealScrollName();
+        string spellJ = SpellJournal::read();
+        int m = spellN.length();
+        int n = spellJ.length();
+        int array[m + 1][n + 1];
 
     // solve for LCS
-    for (int i = 0; i <= m; i++)
-      array[i][0] = 0;
-    for (int j = 0; j <= n; j++)
-      array[0][j] = 0;
-    for (int i = 1; i <= m; i++) {
-      for (int j = 1; j <= n; j++) {
-        if (spellN[i - 1] == spellJ[j - 1])
-          array[i][j] = array[i - 1][j - 1] + 1;
-        else
-          array[i][j] = max(array[i][j - 1], array[i - 1][j]);
-      }
+        
+        for (int i = 0; i <= m; i++)
+        array[i][0] = 0;
+        for (int j = 0; j <= n; j++)
+        array[0][j] = 0;
+        for (int i = 1; i <= m; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+                if (spellN[i - 1] == spellJ[j - 1])
+                    array[i][j] = array[i - 1][j - 1] + 1;
+                else
+                    array[i][j] = max(array[i][j - 1], array[i - 1][j]);
+            }
+        }
+        cout << array[m][n] << endl;
     }
-    cout << array[m][n] << endl;
-  }
 }
 
-class Wizard {
+class Wizard 
+{
     public:
-        Spell *cast() {
+        Spell *cast() 
+        {
             Spell *spell;
             string s; cin >> s;
             int power; cin >> power;
-            if(s == "fire") {
+            if(s == "fire") 
+            {
                 spell = new Fireball(power);
             }
-            else if(s == "frost") {
+            else if(s == "frost") 
+            {
                 spell = new Frostbite(power);
             }
-            else if(s == "water") {
+            else if(s == "water") 
+            {
                 spell = new Waterbolt(power);
             }
-            else if(s == "thunder") {
+            else if(s == "thunder") 
+            {
                 spell = new Thunderstorm(power);
             } 
-            else {
+            else 
+            {
                 spell = new Spell(s);
                 cin >> SpellJournal::journal;
             }
@@ -121,11 +168,13 @@ class Wizard {
         }
 };
 
-int main() {
+int main() 
+{
     int T;
     cin >> T;
     Wizard Arawn;
-    while(T--) {
+    while(T--) 
+    {
         Spell *spell = Arawn.cast();
         counterspell(spell);
     }
