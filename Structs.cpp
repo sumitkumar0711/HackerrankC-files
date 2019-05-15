a504cf637ca0cdfe9adb7fe294357e189e72aa3e
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:40:27 +0530

    Update Structs.cpp

--- Structs.cpp
+++ Structs.cpp
@@ -5,22 +5,29 @@
 #include <algorithm>
 using namespace std;
 
-struct Student {
-  unsigned age{};
-  std::string first_name{};
-  std::string last_name{};
-  unsigned standard{};
+struct Student 
+{
+  unsigned age
+  {};
+  
+  std::string first_name
+  {};
+  
+  std::string last_name
+  {};
+  
+  unsigned standard
+  {};
 };
+
 /*
     add code for struct here.
 */
 
-int main() {
+int main() 
+{
     Student st;
-    
     cin >> st.age >> st.first_name >> st.last_name >> st.standard;
     cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
-    
     return 0;
 }
-
