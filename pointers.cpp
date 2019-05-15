d89a244c556ceceaf9cc12056b687902e41ecd62
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:46:33 +0530

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void update(int *a, int *b) {
+void update(int *a, int *b) 
+{
+
   // Complete this function
+  
   *a = *a + *b;
 
   *b = abs(*a - *b - *b);
 }
 
-int main() {
+int main() 
+{
+
   int a, b;
   int *pa = &a, *pb = &b;
-
   scanf("%d %d", &a, &b);
   update(pa, pb);
   printf("%d\n%d", a, b);
-
   return 0;
 }
-
