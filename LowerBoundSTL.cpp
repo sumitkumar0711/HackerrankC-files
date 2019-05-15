40c2be3f90311a9d07cf9e40e3b52719206de291
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:14:57 +0530

    Update LowerBoundSTL.cpp

--- LowerBoundSTL.cpp
+++ LowerBoundSTL.cpp
@@ -4,22 +4,22 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int main() {
+
+int main() 
+{
   int n;
   cin >> n;
   vector<int> a(n);
   for (int i = 0; i < n; i++)
     cin >> a[i];
-
   int q;
   cin >> q;
-  while (q--) {
+  while (q--) 
+  {
     int x;
     cin >> x;
     auto it = lower_bound(a.begin(), a.end(), x);
     cout << (*it == x ? "Yes " : "No ") << it - a.begin() + 1 << endl;
   }
-
   return 0;
 }
-
