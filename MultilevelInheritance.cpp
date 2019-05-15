6d1561eb84f512fd3da2ab3c9a85610019623663
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:26:21 +0530

    Update MultilevelInheritance.cpp

--- MultilevelInheritance.cpp
+++ MultilevelInheritance.cpp
@@ -1,44 +1,57 @@
 #include <iostream>
 #include <memory>
 
-class Shape {
-public:
-  virtual const void triangle() const = 0;
-  virtual ~Shape() {}
+class Shape
+{
+  public:
+    virtual const void triangle() const = 0;
+    
+    virtual ~Shape() 
+    {}
 };
 
-class Triangle : public Shape {
-public:
-  virtual const void triangle() const override {
-    std::cout << "I am a triangle\n";
-  }
-  virtual ~Triangle() {}
+class Triangle : public Shape 
+{
+  public:
+    virtual const void triangle() const override 
+    {
+      std::cout << "I am a triangle\n";
+    }
+    
+    virtual ~Triangle() 
+    {}
 };
 
-class Isosceles : public Triangle {
-public:
-  virtual const void triangle() const override {
-    std::cout << "I am an isosceles triangle\n";
-  }
-  virtual ~Isosceles() {}
-};
-class Equilateral : public Isosceles {
-public:
-  virtual const void triangle() const override {
-    std::cout << "I am an equilateral triangle\n";
-  }
-  virtual ~Equilateral() {}
+class Isosceles : public Triangle 
+{
+  public:
+    virtual const void triangle() const override 
+    {
+      std::cout << "I am an isosceles triangle\n";
+    }
+    
+    virtual ~Isosceles() 
+    {}
 };
 
-int main() {
+class Equilateral : public Isosceles 
+{
+  public:
+    virtual const void triangle() const override 
+    {
+      std::cout << "I am an equilateral triangle\n";
+    }
+    virtual ~Equilateral() 
+    {}
+};
+
+int main() 
+{
   std::shared_ptr<Shape> obj[3];
-
   obj[0] = std::make_shared<Equilateral>();
   obj[1] = std::make_shared<Isosceles>();
   obj[2] = std::make_shared<Triangle>();
-
   for (auto it : obj)
     it->triangle();
   return 0;
 }
-
