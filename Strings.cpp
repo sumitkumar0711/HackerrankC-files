9a95c8da13a8faa92deaaf4027edd0bab31913e7
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:39:43 +0530

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -4,16 +4,13 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
   string a, b;
   cin >> a >> b;
-
   cout << a.length() << ' ' << b.length() << endl;
-
   cout << a + b << endl;
-
   swap(a[0], b[0]);
   cout << a << ' ' << b << endl;
-
   return 0;
 }
