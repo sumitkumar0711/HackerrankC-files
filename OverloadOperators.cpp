8e9a31d13f5ea26552dc92f2a8582eed8e481f7c
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:29:21 +0530

    Update OverloadOperators.cpp

--- OverloadOperators.cpp
+++ OverloadOperators.cpp
@@ -6,40 +6,46 @@ using namespace std;
 
 class Complex
 {
-public:
-    int a,b;
-    void input(string s)
-    {
-        int v1=0;
-        int i=0;
-        while(s[i]!='+')
+    public:
+        int a,b;
+        void input(string s)
         {
-            v1=v1*10+s[i]-'0';
-            i++;
+            int v1=0;
+            int i=0;
+            while(s[i]!='+')
+            {
+                v1=v1*10+s[i]-'0';
+                i++;
+            }
+            while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+            {
+                i++;
+            }
+            int v2=0;
+            while(i<s.length())
+            {
+                v2=v2*10+s[i]-'0';
+                i++;
+            }
+            a=v1;
+            b=v2;
         }
-        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
-        {
-            i++;
-        }
-        int v2=0;
-        while(i<s.length())
-        {
-            v2=v2*10+s[i]-'0';
-            i++;
-        }
-        a=v1;
-        b=v2;
-    }
 };
-ostream &operator<<(ostream &os, const Complex &c) {
+
+ostream &operator<<(ostream &os, const Complex &c) 
+{
   return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
 }
 
-Complex operator+(const Complex &a, const Complex &b) {
+Complex operator+(const Complex &a, const Complex &b) 
+{
   return {a.a + b.a, a.b + b.b};
 }
+
 //Overload operators + and << for the class complex
+
 //+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+
 //<< should print a complex number in the format "a+ib"
 
 int main()
