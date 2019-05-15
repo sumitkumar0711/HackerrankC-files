efb75356936c0e50c9e13a885939fabc0f1b445b
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 16:47:32 +0530

    Update HotelPrices.cpp

--- HotelPrices.cpp
+++ HotelPrices.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
-class HotelRoom {
+class HotelRoom 
+{
 public:
     HotelRoom(int bedrooms, int bathrooms) 
-    : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
-    
-    int get_price() {
+    : bedrooms_(bedrooms), bathrooms_(bathrooms) 
+    {}
+    int get_price() 
+    {
         return 50*bedrooms_ + 100*bathrooms_;
     }
 private:
@@ -16,42 +18,48 @@ private:
     int bathrooms_;
 };
 
-class HotelApartment : public HotelRoom {
+class HotelApartment : public HotelRoom 
+{
 public:
     HotelApartment(int bedrooms, int bathrooms) 
-    : HotelRoom(bedrooms, bathrooms) {}
-
-    int get_price() {
+    : HotelRoom(bedrooms, bathrooms) 
+    {}
+    int get_price() 
+    {
         return HotelRoom::get_price() + 100;
     }
 };
 
-int main() {
+int main() 
+{
     int n;
     cin >> n;
     vector<HotelRoom*> rooms;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) 
+    {
         string room_type;
         int bedrooms;
         int bathrooms;
         cin >> room_type >> bedrooms >> bathrooms;
-        if (room_type == "standard") {
+        if (room_type == "standard") 
+        {
             rooms.push_back(new HotelRoom(bedrooms, bathrooms));
-        } else {
+        } 
+        else 
+        {
             rooms.push_back(new HotelApartment(bedrooms, bathrooms));
         }
     }
-
     int total_profit = 0;
-    for (auto room : rooms) {
+    for (auto room : rooms) 
+    {
         total_profit += room->get_price();
     }
     cout << total_profit << endl;
-
-    for (auto room : rooms) {
+    for (auto room : rooms) 
+    {
         delete room;
     }
     rooms.clear();
-
     return 0;
 }
