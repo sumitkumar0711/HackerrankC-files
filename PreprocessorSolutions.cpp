5109f63b86f076c3efc6b02a583b39adda18c400
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:36:03 +0530

    Update PreprocessorSolutions.cpp

--- PreprocessorSolutions.cpp
+++ PreprocessorSolutions.cpp
@@ -19,20 +19,22 @@ using namespace std;
 FUNCTION(minimum, <)
 FUNCTION(maximum, >)
 
-int main(){
+int main()
+{
 	int n; cin >> n;
 	vector<int> v(n);
-	foreach(v, i) {
+	foreach(v, i) 
+	{
 		io(v)[i];
 	}
 	int mn = INF;
 	int mx = -INF;
-	foreach(v, i) {
+	foreach(v, i) 
+	{
 		minimum(mn, v[i]);
 		maximum(mx, v[i]);
 	}
 	int ans = mx - mn;
 	cout << toStr(Result =) <<' '<< ans;
 	return 0;
-
 }
