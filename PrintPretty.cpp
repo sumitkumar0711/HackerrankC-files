2a577e5fc0fa675556dee4ee19e49332eabddc1a
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 17:36:32 +0530

    Update PrintPretty.cpp

--- PrintPretty.cpp
+++ PrintPretty.cpp
@@ -2,27 +2,26 @@
 #include <iomanip> 
 using namespace std;
 
-int main() {
+int main() 
+{
 	int T; cin >> T;
 	cout << setiosflags(ios::uppercase);
 	cout << setw(0xf) << internal;
-	while(T--) {
+	while(T--) 
+	{
 		double A; cin >> A;
 		double B; cin >> B;
 		double C; cin >> C;
                 cout << hex << showbase << nouppercase << left;
                 cout << (long long)A << endl;
-
                 cout << setw(15) << showpos << setfill('_') << setprecision(2)
                      << fixed << right;
                 cout << B << endl;
-
                 cout << scientific << setw(15) << setprecision(9) << uppercase
                      << noshowpos;
                 cout << C << endl;
-                /* Enter your code here */
 
+		/* Enter your code here */
 	}
 	return 0;
-
 }
