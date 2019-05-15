e312158d2e2afbacdd31e7cc4a11bc7d7e81ca09
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-05-15 16:41:04 +0530

    Update CppExceptionalHandling.cpp

--- CppExceptionalHandling.cpp
+++ CppExceptionalHandling.cpp
@@ -3,31 +3,44 @@
 
 using namespace std;
 
-int largest_proper_divisor(int n) {
-    if (n == 0) {
+int largest_proper_divisor(int n) 
+{
+    if (n == 0) 
+    {
         throw invalid_argument("largest proper divisor is not defined for n=0");
     }
-    if (n == 1) {
+    if (n == 1) 
+    {
         throw invalid_argument("largest proper divisor is not defined for n=1");
     }
-    for (int i = n/2; i >= 1; --i) {
-        if (n % i == 0) {
+    for (int i = n/2; i >= 1; --i) 
+    {
+        if (n % i == 0) 
+        {
             return i;
         }
     }
-    return -1; // will never happen
+    return -1; 
+    
+    // will never happen
 }
-void process_input(int n) {
-  try {
+
+void process_input(int n) 
+{
+  try 
+  {
     int d = largest_proper_divisor(n);
     cout << "result=" << d << endl;
-  } catch (invalid_argument e) {
+  } 
+  catch (invalid_argument e) 
+  {
     cout << e.what() << endl;
   }
   cout << "returning control flow to caller" << endl;
 }
 
-int main() {
+int main() 
+{
     int n;
     cin >> n;
     process_input(n);
